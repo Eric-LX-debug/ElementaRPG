d198c52623f14253e29e4ff20c6ba2c716578b26
Eric Araujo <eric.araujoxavier@hotmail.com>
2024-12-04 00:02:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll("#menu li").forEach((item) => {
+    item.addEventListener("mouseenter", () => {
+        const dropdown = item.querySelector("ul");
+        if (dropdown) {
+            dropdown.style.display = "block";
+        }
+    });
+
+    item.addEventListener("mouseleave", () => {
+        const dropdown = item.querySelector("ul");
+        if (dropdown) {
+            dropdown.style.display = "none";
+        }
+    });
+});
